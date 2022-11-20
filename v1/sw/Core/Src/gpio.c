9d36e86ccd25a9ae59e51e4ef314d653d2e900ab
Ryan Wheeler <rfwheel@terpmail.umd.edu>
2022-11-19 20:29:14 -0500

    put PA10 in high-speed mode

--- v1/sw/Core/Src/gpio.c
+++ v1/sw/Core/Src/gpio.c
@@ -54,7 +54,7 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Pin = GPIO_PIN_10;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 }
